8b0cf65df7502b7b09d0577edb490ffa4d085dff
JMS <jose.sabater.iglesias@gmail.com>
2024-03-14 22:36:32 +0100

    add exceptions

--- utils/summarization.py
+++ utils/summarization.py
@@ -1,8 +1,6 @@
-from transformers import pipeline
 from transformers import BartTokenizer, BartForConditionalGeneration
 import logging
 import termcolor
-import torch
 
 
 def print_green(text):
@@ -13,7 +11,6 @@ class Summarizer:
     """Helper class for summarizing text using BART"""
 
     def __init__(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
         self.model = BartForConditionalGeneration.from_pretrained(
             "facebook/bart-large-cnn"
@@ -22,11 +19,14 @@ class Summarizer:
 
     def summarize(self, text: str):
         """Create a summary of the input text using BART"""
-        print_green(f"Summarizing: {text[:100]}...")
-        inputs = self.tokenizer(
-            [text], return_tensors="pt", max_length=1024, truncation=True
-        )
-        summary_ids = self.model.generate(
-            inputs["input_ids"], num_beams=2, max_length=100, early_stopping=True
-        )
-        return self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        try:
+            print_green(f"Summarizing: {text[:100]}...")
+            inputs = self.tokenizer(
+                [text], return_tensors="pt", max_length=1024, truncation=True
+            )
+            summary_ids = self.model.generate(
+                inputs["input_ids"], num_beams=2, max_length=100, early_stopping=True
+            )
+            return self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        except Exception as e:
+            print(f"An error occurred during summarization: {str(e)}")
